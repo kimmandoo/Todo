c340fe97cb7648a6dc8cd4ef40d67631c923c66f
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-03-16 21:31:28 +0900

    chore: 커밋확인

--- app/src/main/java/com/challenge/todo/MainActivity.kt
+++ app/src/main/java/com/challenge/todo/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    // 파일 바꼈잖아..
+    // 커밋용 주석
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
