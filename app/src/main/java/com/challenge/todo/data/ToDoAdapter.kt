be6078140141a62d27b68deb64044a7dc6d8f84a
ansehdwls <moondj928@gmail.com>
2024-03-19 22:45:11 +0900

    init : ListAdapter 생성

--- app/src/main/java/com/challenge/todo/data/ToDoAdapter.kt
+++ app/src/main/java/com/challenge/todo/data/ToDoAdapter.kt
@@ -1,4 +1,37 @@
 package com.challenge.todo.data
 
-class ToDoAdapter {
+import android.content.ClipData.Item
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.challenge.todo.databinding.TodoItemBinding
+
+class ToDoAdapter() : ListAdapter<ToDoItem, ToDoAdapter.ToDoViewHolder>(ToDoComporator()){
+
+    class ToDoViewHolder(val binding: TodoItemBinding) : RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    class ToDoComporator : DiffUtil.ItemCallback<ToDoItem>() {
+        override fun areItemsTheSame(oldItem: ToDoItem, newItem: ToDoItem): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: ToDoItem, newItem: ToDoItem): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
 }
