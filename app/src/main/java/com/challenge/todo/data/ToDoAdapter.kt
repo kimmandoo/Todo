ce44a085ac063fe2126932e2a42bda3390ca087d
ansehdwls <moondj928@gmail.com>
2024-03-20 23:23:50 +0900

    fix : CreateViewHolder, onBindViewHolder context 추가, ToDoViewHolder bindInfo 생성

--- app/src/main/java/com/challenge/todo/data/ToDoAdapter.kt
+++ app/src/main/java/com/challenge/todo/data/ToDoAdapter.kt
@@ -1,6 +1,7 @@
 package com.challenge.todo.data
 
 import android.content.ClipData.Item
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.DiffUtil
@@ -11,7 +12,13 @@ import com.challenge.todo.databinding.TodoItemBinding
 class ToDoAdapter() : ListAdapter<ToDoItem, ToDoAdapter.ToDoViewHolder>(ToDoComporator()){
 
     class ToDoViewHolder(val binding: TodoItemBinding) : RecyclerView.ViewHolder(binding.root){
-
+        fun bindInfo(item : ToDoItem){
+            binding.apply {
+                todoIndex.text = position.toString()
+                todoTitle.text = item.title
+                todoContent.text = item.content
+            }
+        }
     }
 
     class ToDoComporator : DiffUtil.ItemCallback<ToDoItem>() {
@@ -26,11 +33,13 @@ class ToDoAdapter() : ListAdapter<ToDoItem, ToDoAdapter.ToDoViewHolder>(ToDoComp
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = TodoItemBinding.inflate(inflater, parent,false)
+        return ToDoViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bindInfo(currentList[position])
     }
 
 
