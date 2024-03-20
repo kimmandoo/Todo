e883b6cee1abc9204aee44de59e310d3703303f0
ansehdwls <moondj928@gmail.com>
2024-03-20 23:21:54 +0900

    style : Top-down 방식으로 data 보기 수정

--- app/src/main/java/com/challenge/todo/data/ToDoItem.kt
+++ app/src/main/java/com/challenge/todo/data/ToDoItem.kt
@@ -1,5 +1,10 @@
 package com.challenge.todo.data
 
-data class ToDoItem (val title : String, val content : String, val registerDate : String, val dueDate : String, val isDone : Boolean ){
+data class ToDoItem (
+    val title : String,
+    val content : String,
+    val registerDate : String,
+    val dueDate : String,
+    val isDone : Boolean ){
 
 }
