34a0bc8bd1f073a5d1bdbf5531435cd6e734985b
kimmandoo <mingyu5675@gmail.com>
2024-03-21 23:09:18 +0900

    feat: option menu 달기

--- app/src/main/java/com/challenge/todo/ui/main/MainActivity.kt
+++ app/src/main/java/com/challenge/todo/ui/main/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.challenge.todo.ui.main
 
-import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import com.challenge.todo.R
 import com.challenge.todo.data.datasource.TodoDatabaseInstance
 import com.challenge.todo.data.dto.Todo
 import com.challenge.todo.databinding.ActivityMainBinding
 import com.challenge.todo.ui.base.BaseActivity
+import com.challenge.todo.ui.detail.DetailActivity
+import kotlin.math.log
 
-
+private const val TAG = "MainActivity"
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
     private val todoAdapter by lazy { TodoAdapter() }
     override val viewModel: MainViewModel by viewModels()
@@ -26,6 +30,7 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.a
                 }
             }
 
+
             mainFab.setOnClickListener {
                 viewModel.insertTodoItem(
                     todoDao = todoDBInstance.todoDao(),
@@ -34,4 +39,34 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.a
             }
         }
     }
+    private val request =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+
+            }
+        }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_option, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.option_done->{
+                Log.d(TAG, "onOptionsItemSelected: ")
+                request.launch(Intent(baseContext, DetailActivity::class.java))
+            }
+            R.id.option_todo->{
+
+            }
+            R.id.option_all ->{
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
