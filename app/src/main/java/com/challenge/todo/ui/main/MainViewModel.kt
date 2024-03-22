4285166a5fa6cfc7e01e1065ea57e40a237dbf7b
ansehdwls <moondj928@gmail.com>
2024-03-22 23:09:43 +0900

    fix : MainViewModel 결합 , checkBox, item click listener 기능 추가 필요

--- app/src/main/java/com/challenge/todo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/challenge/todo/ui/main/MainViewModel.kt
@@ -4,10 +4,19 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.challenge.todo.data.ToDoItem
+import java.time.LocalDateTime
 
 class MainViewModel : ViewModel() {
-    private val _toDoList = MutableLiveData<List<ToDoItem>>()
-    val toDoList : LiveData<List<ToDoItem>> get() = _toDoList
+    val _toDoList = MutableLiveData<MutableList<ToDoItem>>()
+    private val toDoList = arrayListOf<ToDoItem>()
 
+    fun toggleToDoItem(toDoItem: ToDoItem){
+        toDoItem.isDone = !toDoItem.isDone
+        _toDoList.value = toDoList
+    }
 
+    fun checkButton(toDoItem: ToDoItem){
+        toDoItem.dueDate = LocalDateTime.now().toString()
+        _toDoList.value = toDoList
+    }
 }
