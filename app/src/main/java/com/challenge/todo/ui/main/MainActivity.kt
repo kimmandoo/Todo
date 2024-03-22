a5b6d1b9c4f6740a1fd7264b2c9176b0075d4ea5
kimmandoo <mingyu5675@gmail.com>
2024-03-22 23:06:17 +0900

    feat: option menu 달기, callback으로 itemclick 구현

--- app/src/main/java/com/challenge/todo/ui/main/MainActivity.kt
+++ app/src/main/java/com/challenge/todo/ui/main/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.challenge.todo.ui.main
 
-import android.content.Intent
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import com.challenge.todo.R
@@ -11,25 +11,30 @@ import com.challenge.todo.data.datasource.TodoDatabaseInstance
 import com.challenge.todo.data.dto.Todo
 import com.challenge.todo.databinding.ActivityMainBinding
 import com.challenge.todo.ui.base.BaseActivity
-import com.challenge.todo.ui.detail.DetailActivity
-import kotlin.math.log
 
 private const val TAG = "MainActivity"
+
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
-    private val todoAdapter by lazy { TodoAdapter() }
+    private val todoAdapter by lazy {
+        TodoAdapter(onTodoItemClick = { todo: Todo ->
+            showTodoDetail(todo)
+        })
+    }
+
     override val viewModel: MainViewModel by viewModels()
 
     override fun initView() {
         val todoDBInstance = TodoDatabaseInstance.getDatabase(context = applicationContext)
         binding.apply {
             adapter = todoAdapter
+            setSupportActionBar(mainToolbar)
+
             viewModel.getListFromRoomDB(todoDao = todoDBInstance.todoDao())
             lifecycleOwner?.let { lifecycleOwner ->
                 viewModel.todoList.observe(lifecycleOwner) { todoList ->
                     adapter?.submitList(todoList)
                 }
             }
-
 
             mainFab.setOnClickListener {
                 viewModel.insertTodoItem(
@@ -39,6 +44,11 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.a
             }
         }
     }
+
+    private fun showTodoDetail(todo: Todo) {
+        Toast.makeText(this, todo.toString(), Toast.LENGTH_SHORT).show()
+    }
+
     private val request =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == RESULT_OK) {
@@ -46,26 +56,28 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.a
             }
         }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_option, menu)
-        return super.onCreateOptionsMenu(menu)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_option, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.option_done->{
-                Log.d(TAG, "onOptionsItemSelected: ")
-                request.launch(Intent(baseContext, DetailActivity::class.java))
-            }
-            R.id.option_todo->{
+        when (item.itemId) {
+            R.id.option_done -> {
+                Log.d(TAG, "onOptionsItemSelected: done")
 
             }
-            R.id.option_all ->{
+
+            R.id.option_todo -> {
+                Log.d(TAG, "onOptionsItemSelected: done")
+
+            }
+
+            R.id.option_all -> {
+                Log.d(TAG, "onOptionsItemSelected: done")
 
             }
         }
-
         return super.onOptionsItemSelected(item)
     }
 
--- app/src/main/java/com/challenge/todo/ui/main/TodoViewHolder.kt
+++ app/src/main/java/com/challenge/todo/ui/main/TodoViewHolder.kt
@@ -1,19 +1,22 @@
 package com.challenge.todo.ui.main
 
 import android.view.View
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.challenge.todo.data.dto.Todo
 import com.challenge.todo.databinding.ItemTodoMainBinding
 
-class TodoViewHolder(private val binding: ItemTodoMainBinding) :
+class TodoViewHolder(
+    private val binding: ItemTodoMainBinding,
+    private val onTodoItemClick: (todo: Todo) -> Unit = { _ -> }
+) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(item: Todo) {
         binding.apply {
+            itemView.setOnClickListener {
+                onTodoItemClick(item)
+            }
+
             with(item) {
-                itemTodo.setOnClickListener {
-                    Toast.makeText(binding.root.context, this.date, Toast.LENGTH_SHORT).show()
-                }
                 itemTvTitle.apply {
                     text = title
                 }
