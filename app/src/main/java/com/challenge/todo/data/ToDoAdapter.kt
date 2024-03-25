89dec936dad8b2eeef2112b139e38f7ad5bea9bd
ansehdwls <moondj928@gmail.com>
2024-03-25 23:18:33 +0900

    fix : Context menu 추가 중

--- app/src/main/java/com/challenge/todo/data/ToDoAdapter.kt
+++ app/src/main/java/com/challenge/todo/data/ToDoAdapter.kt
@@ -1,31 +1,32 @@
 package com.challenge.todo.data
 
-import android.content.ClipData.Item
-import android.content.Context
+import android.util.Log
 import android.view.ContextMenu
 import android.view.LayoutInflater
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.challenge.todo.R
 import com.challenge.todo.databinding.TodoItemBinding
 
-class ToDoAdapter(private var toDoList : MutableList<ToDoItem>,
-                  val onClickCheckButton : (toDoItem : ToDoItem) -> Unit,
-                  val menuInflater: MenuInflater,
-                  val context: Context
-)
-    : RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder>() {
+private const val TAG = "ToDoAdapter"
+class ToDoAdapter(var toDoList: MutableList<ToDoItem>,
+                  val onClickCheckButton: (toDoItem: ToDoItem) -> Unit,
+                  val menuInflater: MenuInflater
+) : RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder>() {
 
     class ToDoViewHolder(val binding: TodoItemBinding,
                          val menuInflater: MenuInflater,
-                         val itemClickListener: ItemClickListener) :
+                         val itemClickListener: ItemClickListener?) :
         RecyclerView.ViewHolder(binding.root),
         View.OnCreateContextMenuListener {
+
+        init {
+            itemView.setOnCreateContextMenuListener(this)
+        }
+
         fun bindInfo(item: ToDoItem) {
             binding.apply {
                 todoIndex.text = position.toString()
@@ -42,13 +43,17 @@ class ToDoAdapter(private var toDoList : MutableList<ToDoItem>,
             menuInflater.inflate(R.menu.delete_cocntext_menu, menu)
             menu?.findItem(R.id.todo_delete)?.setOnMenuItemClickListener {
                 val position = adapterPosition
-                if( position != RecyclerView.NO_POSITION){
-                    itemClickListener.deleteItem(position)
+                if (position != RecyclerView.NO_POSITION) {
+                    Log.d(TAG, "onCreateContextMenu: ${position}")
+                    Log.d(TAG, "itemClickListener: ${itemClickListener}")
+                    itemClickListener?.deleteItem(position)
                 }
-
                 true
             }
+        }
 
+        fun getItemPosition(): Int { // position 프로퍼티의 getter 이름 변경
+            return adapterPosition
         }
     }
 
@@ -60,28 +65,25 @@ class ToDoAdapter(private var toDoList : MutableList<ToDoItem>,
         override fun areContentsTheSame(oldItem: ToDoItem, newItem: ToDoItem): Boolean {
             return oldItem == newItem
         }
-
     }
 
-    fun setData(newData : MutableList<ToDoItem>){
-        toDoList = newData
+    fun setData(newData: List<ToDoItem>) { // List<ToDoItem>로 수정
+        toDoList.clear()
+        toDoList.addAll(newData)
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int,
-                                    ): ToDoViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = TodoItemBinding.inflate(inflater, parent,false)
-
-
-        return ToDoViewHolder(view,menuInflater,itemClickListener)
+        val view = TodoItemBinding.inflate(inflater, parent, false)
+        if(::itemClickListener.isInitialized) return ToDoViewHolder(view, menuInflater, itemClickListener)
+        return ToDoViewHolder(view, menuInflater, null)
     }
-
 
     override fun getItemCount(): Int = toDoList.size
 
     override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
+        holder.getItemPosition()// 위치 설정
         holder.bindInfo(toDoList[position])
 
         holder.binding.floatingActionButton1.apply {
@@ -98,5 +100,4 @@ class ToDoAdapter(private var toDoList : MutableList<ToDoItem>,
         fun OnClick(view: View, position: Int)
         fun deleteItem(position: Int)
     }
-
 }
