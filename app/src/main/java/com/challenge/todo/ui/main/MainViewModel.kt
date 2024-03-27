c03f371c4fd1bc499729d94f3a4268f5929bebc2
ansehdwls <moondj928@gmail.com>
2024-03-27 23:04:48 +0900

    fix : ViewModel Factory 구현, insert, select 구현중

--- app/src/main/java/com/challenge/todo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/challenge/todo/ui/main/MainViewModel.kt
@@ -1,28 +1,39 @@
 package com.challenge.todo.ui.main
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.challenge.todo.data.ToDoItem
-import java.time.LocalDateTime
+import com.challenge.todo.data.db.entity.ToDoEntity
+import com.challenge.todo.data.db.repo.ToDoRepo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
 
-class MainViewModel : ViewModel() {
-    val _toDoList = MutableLiveData<MutableList<ToDoItem>>()
-    private val toDoList = arrayListOf<ToDoItem>()
-
-    fun toggleToDoItem(toDoItem: ToDoItem){
-        toDoItem.isDone = !toDoItem.isDone
-        _toDoList.value = toDoList
+private const val TAG = "MainViewModel"
+class MainViewModel(private val toDoRepo: ToDoRepo) : ViewModel() {
+   private val _todolist = MutableLiveData<List<ToDoEntity>>()
+    val toDoList : LiveData<List<ToDoEntity>>  = _todolist
+    fun insert(toDoItem: ToDoEntity) = viewModelScope.launch(Dispatchers.IO) {
+        toDoRepo.insertToDo(toDoItem)
     }
-
-    fun checkButton(toDoItem: ToDoItem){
-        toDoItem.dueDate = LocalDateTime.now().toString()
-        _toDoList.value = toDoList
-    }
-
-    fun deleteItem(position: Int) {
-        toDoList.removeAt(position)
-        _toDoList.value = toDoList
+    fun select(id : Int) = viewModelScope.launch(Dispatchers.IO) {
+        toDoRepo.getTodoItemDetail(id)
     }
 }
+
+class MainViewModelFactory(private val repo: ToDoRepo) : ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(MainViewModel::class.java)){
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(repo) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+
