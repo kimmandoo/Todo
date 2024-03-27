1523aa674b5abe507e2eee463f21c7e55a459cf2
ansehdwls <moondj928@gmail.com>
2024-03-27 23:02:53 +0900

    fix : ListAdapter로 변경 , LiveData 사용

--- app/src/main/java/com/challenge/todo/data/ToDoAdapter.kt
+++ app/src/main/java/com/challenge/todo/data/ToDoAdapter.kt
@@ -6,96 +6,57 @@ import android.view.LayoutInflater
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.challenge.todo.R
+import com.challenge.todo.data.db.entity.ToDoEntity
 import com.challenge.todo.databinding.TodoItemBinding
+import com.challenge.todo.ui.todo_detail.TodoDetailDialog
 
 private const val TAG = "ToDoAdapter"
-class ToDoAdapter(var toDoList: MutableList<ToDoItem>,
-                  val onClickCheckButton: (toDoItem: ToDoItem) -> Unit,
-                  val menuInflater: MenuInflater
-) : RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder>() {
-
-    class ToDoViewHolder(val binding: TodoItemBinding,
-                         val menuInflater: MenuInflater,
-                         val itemClickListener: ItemClickListener?) :
-        RecyclerView.ViewHolder(binding.root),
-        View.OnCreateContextMenuListener {
-
-        init {
-            itemView.setOnCreateContextMenuListener(this)
-        }
-
-        fun bindInfo(item: ToDoItem) {
-            binding.apply {
-                todoIndex.text = position.toString()
-                todoTitle.text = item.title
-                todoContent.text = item.content
-            }
-        }
-
-        override fun onCreateContextMenu(
-            menu: ContextMenu?,
-            v: View?,
-            menuInfo: ContextMenu.ContextMenuInfo?
-        ) {
-            menuInflater.inflate(R.menu.delete_cocntext_menu, menu)
-            menu?.findItem(R.id.todo_delete)?.setOnMenuItemClickListener {
-                val position = adapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    Log.d(TAG, "onCreateContextMenu: ${position}")
-                    Log.d(TAG, "itemClickListener: ${itemClickListener}")
-                    itemClickListener?.deleteItem(position)
-                }
-                true
-            }
-        }
-
-        fun getItemPosition(): Int { // position 프로퍼티의 getter 이름 변경
-            return adapterPosition
-        }
+class ToDoAdapter() : ListAdapter<ToDoEntity, ToDoAdapter.ToDoViewHolder>(ToDoComparator()){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
+        return ToDoViewHolder.create(parent)
     }
 
-    class ToDoComporator : DiffUtil.ItemCallback<ToDoItem>() {
-        override fun areItemsTheSame(oldItem: ToDoItem, newItem: ToDoItem): Boolean {
+    override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current.title, current.content)
+    }
+
+    class ToDoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val toDoTitleView : TextView = itemView.findViewById(R.id.todo_title)
+        private val toDoContentView : TextView = itemView.findViewById(R.id.todo_content)
+
+        fun bind(title : String?, content : String?){
+            toDoTitleView.text = title
+            toDoContentView.text = content
+        }
+        companion object{
+            fun create(parent: ViewGroup) : ToDoViewHolder{
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.todo_item,parent,false)
+                return ToDoViewHolder(view)
+            }
+        }
+
+    }
+
+    class ToDoComparator : DiffUtil.ItemCallback<ToDoEntity>(){
+        override fun areItemsTheSame(oldItem: ToDoEntity, newItem: ToDoEntity): Boolean {
             return oldItem === newItem
         }
 
-        override fun areContentsTheSame(oldItem: ToDoItem, newItem: ToDoItem): Boolean {
-            return oldItem == newItem
+        override fun areContentsTheSame(oldItem: ToDoEntity, newItem: ToDoEntity): Boolean {
+            return oldItem.content == newItem.content
         }
+
     }
-
-    fun setData(newData: List<ToDoItem>) { // List<ToDoItem>로 수정
-        toDoList.clear()
-        toDoList.addAll(newData)
-        notifyDataSetChanged()
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val view = TodoItemBinding.inflate(inflater, parent, false)
-        if(::itemClickListener.isInitialized) return ToDoViewHolder(view, menuInflater, itemClickListener)
-        return ToDoViewHolder(view, menuInflater, null)
-    }
-
-    override fun getItemCount(): Int = toDoList.size
-
-    override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
-        holder.getItemPosition()// 위치 설정
-        holder.bindInfo(toDoList[position])
-
-        holder.binding.floatingActionButton1.apply {
-            setOnClickListener {
-                onClickCheckButton.invoke(toDoList[position])
-            }
-            check(true)
-        }
-    }
-
-    lateinit var itemClickListener: ItemClickListener
-
+    lateinit var itemClickListner: ItemClickListener
     interface ItemClickListener {
         fun OnClick(view: View, position: Int)
         fun deleteItem(position: Int)
